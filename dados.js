4b0a7f31fe8d5ac112936347dfde1ec414fa21fa
EduKalil <edukalil87@gmail.com>
2024-09-06 20:03:59 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo : "Rayssa Leal",
         descricao : "A skatista mirim que encantou o mundo. Rayssa Leal, conhecida como 'Fadinha', é uma das maiores promessas do skate mundial. Com apenas [idade], ela já conquistou diversos títulos e se tornou um ícone para as novas gerações.",
-        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags : "skate fada sk8"
     },  
     {
         titulo: "Beatriz Souza",
         descricao: "Atleta de Jiu-Jitsu, Beatriz Souza se destaca pela sua técnica e agilidade. Campeã em diversas competições, ela é considerada uma das maiores promessas da modalidade.",
-        link: "https://exemplo.com/beatrizsouza" // Substitua por um link válido para Beatriz Souza
+        link: "https://exemplo.com/beatrizsouza", // Substitua por um link válido para Beatriz Souza
+        tags : "judo judô judoca"
     },
     {
         titulo: "Rebeca Andrade",
         descricao: "Ginasta brasileira, Rebeca Andrade conquistou o coração dos brasileiros com suas performances impecáveis. Medalhista olímpica, ela é um exemplo de determinação e superação.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade" 
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "ginasta ginastica ouro"
     }
-];
+]
